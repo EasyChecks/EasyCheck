dfe13c6cc287aec95a6c0199fdd3fe2861385f78
SaBo4K <notinw001@gmail.com>
2025-10-14 00:44:25 +0700

    Update routing and add new pages structure

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,19 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
+// ✅ สำหรับ React Router ให้ redirect ทุก path กลับ index.html
 export default defineConfig({
-  plugins: [react(),],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  server: {
+    historyApiFallback: true, // 👈 สำคัญมาก
+  },
+  preview: {
+    historyApiFallback: true, // 👈 สำหรับตอน build แล้ว preview
+  },
+});
