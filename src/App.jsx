d579a7917e93269a6274634e813f2c2b31582966
allmusgus <t.niemsuwan@gmail.com>
2025-10-14 00:34:18 +0700

    Add Auth to App & create login & reset password in Auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
 import React from 'react'
 import 'cally'
 import Button from './components/ui/button'
-import { Calendar } from "./components/ui/calendar"
+import Auth from './pages/Auth/Auth'
 
 function App() {
   return (
     <>
-      <Button variant="default">Hello</Button>
-      <div>App GGEZ asdsadsadsadsa</div>
-      <div>App GGEZ
-        <div>YEDHODMAK</div>
-      </div>
-      <div>App GGEZ</div>
-      <div>Hello I'm Nigg</div>
-      <div>hjh</div>
+      <Auth />
     </>
   )
 }
