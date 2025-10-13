46f946f394d4ba961f679ec471422e14c04cffca
hazeco <thunderwolf2209@gmail.com>
2025-10-13 22:17:05 +0700

    update folder

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import {
   createBrowserRouter,
   RouterProvider
 } from 'react-router-dom'
+import React from "react"
 
 const router = createBrowserRouter([
   { path: '/', 
@@ -14,7 +15,7 @@ const router = createBrowserRouter([
 ])
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <React.StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
+  </React.StrictMode>,
 )
