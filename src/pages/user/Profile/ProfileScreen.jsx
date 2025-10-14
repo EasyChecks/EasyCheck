e2427c0f6acfa8e5ecf6551a0e603654149ce5c7
hazeco <thunderwolf2209@gmail.com>
2025-10-14 22:34:55 +0700

    fix ProfileScreen

--- src/pages/user/Profile/ProfileScreen.jsx
+++ src/pages/user/Profile/ProfileScreen.jsx
@@ -2,6 +2,23 @@ import React from 'react';
 import { IoArrowBack, IoLockClosedOutline, IoPowerOutline, IoExitOutline } from 'react-icons/io5';
 import { HiOutlineOfficeBuilding } from 'react-icons/hi';
 
+const data = [
+                {  
+                  id: 1, 
+                  name: "Mr.NiggaTron", 
+                  position: "เจ้าหน้าที่คนดำ", 
+                  department: "JUBJUB", 
+                  profilePic: "/images/niggatron.jpg" 
+                  }
+              ];
+const datacrop = [
+                {  
+                  id: 1, 
+                  address: "88 อาคารดีทัค ทาวเวอร์ 2 ยูนิต 1507 ชั้น 15 ถ.เทคโนโลยี <br/> แขวง บางเขน เขตบางเขน กรุงเทพมหานคร 10220",
+                  callcenter: "02-456-7890",
+                  email: "contact@pineapplesolutions.com"
+                }
+              ];
 
 function ProfileScreen() {
   return (
@@ -13,14 +30,14 @@ function ProfileScreen() {
         <div className="flex flex-col items-center">
 
           <img
-            src="/images/niggatron.jpg"
+            src="{data.profilePic}"
             alt="Profile Picture"
             className="object-cover w-24 h-24 mb-4 border-4 border-white rounded-full"
           />
         {/* ข้อมูลพนักงาน */}
-          <h1 className="text-xl font-bold">Mr.NiggaTron</h1>
-          <p className="text-sm text-blue-100">เจ้าหน้าที่คนดำ</p>
-          <p className="mt-1 text-xs text-blue-200">JUBJUB</p>
+          <h1 className="text-xl font-bold">{data.name}</h1>
+          <p className="text-sm text-blue-100">{data.position}</p>
+          <p className="mt-1 text-xs text-blue-200">{data.department}</p>
         </div>
       </header>
 
@@ -31,6 +48,8 @@ function ProfileScreen() {
           <div className="flex items-center justify-between p-4 cursor-pointer hover:bg-slate-50">
             <div className="flex items-center">
               <IoLockClosedOutline className="mr-4 text-xl text-slate-500" />
+              {/* Link to auth(resetpassword) */}
+
               <span className="text-slate-800">ตั้งรหัสผ่าน</span>
             </div>
             <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 text-slate-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -46,27 +65,26 @@ function ProfileScreen() {
             <div>
               <h2 className="font-bold text-slate-800">Pineapple Solutions Co., Ltd.</h2>
               <p className="mt-2 text-sm text-slate-600">
-                88 อาคารดีทัค ทาวเวอร์ 2 ยูนิต 1507 ชั้น 15 ถ.เทคโนโลยี <br/>
-                แขวง บางเขน เขตบางเขน กรุงเทพมหานคร 10220
+                {datacrop.address}
               </p>
-              <p className="mt-2 text-sm text-slate-600">Call Center : 02-456-7890</p>
-              <p className="mt-1 text-sm text-slate-600">Email : contact@pineapplesolutions.com</p>
+              <p className="mt-2 text-sm text-slate-600">{`Call Center : ${datacrop.callcenter}`}</p>
+              <p className="mt-1 text-sm text-slate-600">{`Email : ${datacrop.email}`}</p>
             </div>
           </div>
         </div>
 
         {/* ออกจากระบบ */}
         <div className="mt-4 overflow-hidden bg-white rounded-lg shadow-sm">
-           <div className="flex items-center justify-between p-4 cursor-pointer hover:bg-slate-50">
+          <div className="flex items-center justify-between p-4 cursor-pointer hover:bg-slate-50">
             <div className="flex items-center">
               <IoPowerOutline className="mr-4 text-xl text-red-500" />
+              {/* link to dashboarduser */}
               <span className="font-medium text-red-500">ออกจากระบบ</span>
             </div>
             <IoExitOutline className="text-xl text-slate-400" />
           </div>
         </div>
       </main>
-
     </div>
   );
 }
