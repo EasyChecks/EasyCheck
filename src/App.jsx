1c617ed163c79984cd843bc443765a4915d2084e
hazeco <thunderwolf2209@gmail.com>
2025-10-14 20:50:13 +0700

    updated app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import 'cally'
 import Button from './components/ui/button'
-import Auth from './pages/Auth/Auth'
 import { Calendar } from "./components/ui/calendar"
 import Nav from './components/user/nav/Nav'
 import Auth from './pages/Auth/Auth'
