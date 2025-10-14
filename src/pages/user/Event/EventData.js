d0fbbba9be2a66cb69cc2c8c21f4cfa9102e7752
SaBo4K <notinw001@gmail.com>
2025-10-14 23:15:58 +0700

    feat: add event management components and data structure

---
+++ src/pages/user/Event/EventData.js
@@ -0,0 +1,37 @@
+const eventList = [
+  {
+    id: 1,
+    title: "คุยงานกับลูกค้า",
+    person: "คุณธิติกร",
+    location: "โบกเทคบางนา",
+    dateStart: "2025-09-23",
+    dateEnd: "2025-09-24",
+    time: "09:00 - 11:00",
+    description: "เก็บ requirements ของลูกค้า",
+    status: "ยังไม่เข้าร่วม",
+  },
+  {
+    id: 2,
+    title: "ลงตรวจไซต์งาน",
+    person: "คุณภัทรพล",
+    location: "Site งานโครงการ A",
+    dateStart: "2025-09-25",
+    dateEnd: "2025-09-25",
+    time: "13:00 - 15:00",
+    description: "ตรวจสอบพื้นที่และวัดขนาดติดตั้ง",
+    status: "ลา",
+  },
+  {
+    id: 3,
+    title: "ติดตั้งสินค้าที่บ้านลูกค้า",
+    person: "คุณเกียรติศักดิ์",
+    location: "หมู่บ้าน The Grand",
+    dateStart: "2025-09-26",
+    dateEnd: "2025-09-26",
+    time: "09:00 - 17:00",
+    description: "ติดตั้งระบบอุปกรณ์ลูกค้า",
+    status: "เข้าร่วมแล้ว",
+  },
+];
+
+export default eventList;
