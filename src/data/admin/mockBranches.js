6f454f08073fd902561faf9da4cc37376dd08701
Khetsophon Ainautoon <thunderwolf2209@gmail.com>
2025-11-27 22:47:49 +0700

    Update mockBranches.js

--- src/data/admin/mockBranches.js
+++ src/data/admin/mockBranches.js
@@ -4,7 +4,6 @@
 
 export const mockBranches = [
   { id: 'BKK101', name: 'BKK 101 (สยาม)', provinceCode: 'BKK', branchCode: '101' },
-  { id: 'BKK102', name: 'BKK 102 (อโศก)', provinceCode: 'BKK', branchCode: '102' },
   { id: 'CNX201', name: 'CNX (เชียงใหม่)', provinceCode: 'CNX', branchCode: '201' },
   { id: 'PKT301', name: 'PKT (ภูเก็ต)', provinceCode: 'PKT', branchCode: '301' },
 ];
